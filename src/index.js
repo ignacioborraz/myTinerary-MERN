48708e100f7846b5ddbaad133b9edec8534bdb93
ignacioborraz <ignacioborraz@hotmail.com>
2022-02-16 18:07:19 -0300

    settings & pre-navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
